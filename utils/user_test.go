d4ff445c27450d72d364cfbb683e4c1d5e2c0a2f
nezhibitskiy <cross01rus@icloud.com>
2022-03-08 23:50:44 +0300

    Started developing userCreate test

--- utils/user_test.go
+++ utils/user_test.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"github.com/stretchr/testify/assert"
+	"myapp/db"
+	"myapp/mockDB"
+	"myapp/models"
 	"testing"
 )
 
@@ -60,4 +63,37 @@ func TestPassword(t *testing.T) {
 			assert.Equal(t, c.err, err)
 		})
 	}
+}
+
+func TestAddUsers(t *testing.T) {
+	mockDB.MockedDB(mockDB.CREATE)
+	defer mockDB.MockedDB(mockDB.DROP)
+
+	//newUser := "fooo"
+	//database.AddUser(newUser)
+
+	/*
+	   As we know ConnectDB() will connect database named DATABASE_NAME defined in .env,
+	   make sure to change that before running tests.
+	*/
+
+	database, err := db.ConnectDB()
+	if err != nil {
+		t.Errorf("Error connecting database in %v\n%v", t.Name(), err)
+	}
+	defer database.Close()
+
+	newUserInDB := models.User{
+		Name:     "user1",
+		Email:    "mail@mai.ru",
+		Password: "passwordAZAZA123",
+	}
+
+	if _, err = CreateUser(database, newUserInDB); err != nil {
+		t.Errorf("%v wasn't added.", newUserInDB)
+	}
+
+	//if notFound {
+	//	t.Errorf("%v was added but was not retrieved.", newUser)
+	//}
 }
