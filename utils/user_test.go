cf7b1e499cacb0830ef9d28a7f9e116e06fed72b
nezhibitskiy <cross01rus@icloud.com>
2022-03-09 16:57:24 +0300

    Removed extra unworkable test

--- utils/user_test.go
+++ utils/user_test.go
@@ -1,18 +1,8 @@
 package utils
 
 import (
-	"context"
-	"fmt"
-	"github.com/jackc/pgmock"
-	"github.com/jackc/pgproto3/v2"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"myapp/models"
-	"net"
-	"strings"
 	"testing"
-	"time"
 )
 
 func TestPassword(t *testing.T) {
@@ -70,115 +60,4 @@ func TestPassword(t *testing.T) {
 			assert.Equal(t, c.err, err)
 		})
 	}
-}
-
-func TestAddUsers(t *testing.T) {
-	script := &pgmock.Script{
-		Steps: pgmock.AcceptUnauthenticatedConnRequestSteps(),
-	}
-	script.Steps = append(script.Steps, pgmock.ExpectMessage(&pgproto3.Query{String: "SELECT * FROM users WHERE email=$1;"}))
-	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.RowDescription{
-		Fields: []pgproto3.FieldDescription{
-			pgproto3.FieldDescription{
-				Name:                 []byte("id"),
-				TableOID:             0,
-				TableAttributeNumber: 0,
-				DataTypeOID:          23,
-				DataTypeSize:         4,
-				TypeModifier:         -1,
-				Format:               0,
-			},
-			pgproto3.FieldDescription{
-				Name:                 []byte("username"),
-				TableOID:             0,
-				TableAttributeNumber: 0,
-				DataTypeOID:          23,
-				DataTypeSize:         4,
-				TypeModifier:         -1,
-				Format:               0,
-			},
-			pgproto3.FieldDescription{
-				Name:                 []byte("password"),
-				TableOID:             0,
-				TableAttributeNumber: 0,
-				DataTypeOID:          23,
-				DataTypeSize:         4,
-				TypeModifier:         -1,
-				Format:               0,
-			},
-			pgproto3.FieldDescription{
-				Name:                 []byte("salt"),
-				TableOID:             0,
-				TableAttributeNumber: 0,
-				DataTypeOID:          23,
-				DataTypeSize:         4,
-				TypeModifier:         -1,
-				Format:               0,
-			},
-		},
-	}))
-	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.DataRow{
-		Values: [][]byte{[]byte("0"), []byte("name1"), []byte("email1@email.net"), []byte("password"), []byte("salt")},
-	}))
-	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.CommandComplete{CommandTag: []byte("SELECT * FROM users WHERE email=$1;")}))
-	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.ReadyForQuery{TxStatus: 'I'}))
-	script.Steps = append(script.Steps, pgmock.ExpectMessage(&pgproto3.Terminate{}))
-
-	ln, err := net.Listen("tcp", "127.0.0.1:")
-	require.NoError(t, err)
-	defer ln.Close()
-
-	serverErrChan := make(chan error, 1)
-	go func() {
-		defer close(serverErrChan)
-
-		conn, err := ln.Accept()
-		if err != nil {
-			serverErrChan <- err
-			return
-		}
-		defer conn.Close()
-
-		err = conn.SetDeadline(time.Now().Add(time.Second))
-		if err != nil {
-			serverErrChan <- err
-			return
-		}
-
-		err = script.Run(pgproto3.NewBackend(pgproto3.NewChunkReader(conn), conn))
-		if err != nil {
-			serverErrChan <- err
-			return
-		}
-	}()
-
-	parts := strings.Split(ln.Addr().String(), ":")
-	host := parts[0]
-	port := parts[1]
-	connStr := fmt.Sprintf("sslmode=disable host=%s port=%s", host, port)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	dbPool, err := pgxpool.Connect(ctx, connStr)
-	defer dbPool.Close()
-
-	//pgConn, err := pgconn.Connect(ctx, connStr)
-	//require.NoError(t, err)
-
-	results, err := IsUserUnique(dbPool, models.User{Email: "email1@email.com"})
-
-	//results, err := pgConn.Exec(ctx, "select 42").ReadAll()
-
-	assert.NoError(t, err)
-
-	assert.Len(t, results, 1)
-	//assert.Nil(t, results[0].Err)
-	//assert.Equal(t, "SELECT 1", string(results[0].CommandTag))
-	//assert.Len(t, results[0].Rows, 1)
-	//assert.Equal(t, "42", string(results[0].Rows[0][0]))
-
-	//pgConn.Close(ctx)
-
-	assert.NoError(t, <-serverErrChan)
 }
