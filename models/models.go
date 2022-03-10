aa4208df532226bf5adc862abb182ef28fc26eae
nezhibitskiy <cross01rus@icloud.com>
2022-03-10 12:18:49 +0300

    Added _- and other symbols in regexp

--- models/models.go
+++ models/models.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	ID       int64  `json:"id" form:"id"`
 	Name     string `json:"username" form:"username" validate:"nonzero" example:"name"`
-	Email    string `json:"email" form:"email" validate:"regexp=^[0-9a-zA-Z]+@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]+)+$" example:"email@email.com"`
+	Email    string `json:"email" form:"email" validate:"regexp=^[0-9a-zA-Z!#$%&'*+/=?^_{|}~-]+@[0-9a-zA-Z+/=?^_{|}~-]+(\\.[0-9a-zA-Z+/=?^_{|}~-]+)+$" example:"email@email.com"`
 	Password string `json:"password" form:"password" validate:"min=8" example:"password"`
 	Salt     string `json:"salt" form:"salt"`
 }
