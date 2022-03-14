309412dfcaf8665935b778d11112d925369abc37
nezhibitskiy <cross01rus@icloud.com>
2022-03-14 15:06:26 +0300

    Added genre in movie random

--- internal/adapters/db/movie/storage.go
+++ internal/adapters/db/movie/storage.go
@@ -30,20 +30,37 @@ func (ms *movieStorage) GetOne(id int) (*domain.Movie, error) {
 }
 
 func (ms *movieStorage) GetRandom(limit int) ([]domain.Movie, error) {
-	sql := "SELECT id, name, description, picture, video, trailer FROM movies ORDER BY RANDOM() LIMIT $1"
+	sql := "SELECT id, name, description, picture, video, trailer FROM movies " +
+		"ORDER BY RANDOM() LIMIT $1"
 
 	selectedMovies := make([]domain.Movie, 0, limit)
 
 	rows, err := ms.db.Query(context.Background(), sql, limit)
+
 	if err != nil {
 		return nil, err
 	}
-	var singleMovie domain.Movie
 	for rows.Next() {
+		var singleMovie domain.Movie
 		if err = rows.Scan(&singleMovie.ID, &singleMovie.Name, &singleMovie.Description, &singleMovie.Picture,
 			&singleMovie.Video, &singleMovie.Trailer); err != nil {
 			return nil, err
 		}
+		sql2 := "SELECT g.name FROM genre AS g JOIN movies_genre mv_g ON mv_g.genre_id = g.id " +
+			"WHERE mv_g.movie_id = $1"
+		rows2, err2 := ms.db.Query(context.Background(), sql2, singleMovie.ID)
+		if err2 != nil {
+			singleMovie.Genre = append(singleMovie.Genre, err.Error())
+		}
+		for rows2.Next() {
+			var genre string
+			if err2 = rows2.Scan(&genre); err2 != nil {
+				singleMovie.Genre = append(singleMovie.Genre, err.Error())
+				break
+			}
+			singleMovie.Genre = append(singleMovie.Genre, genre)
+		}
+
 		selectedMovies = append(selectedMovies, singleMovie)
 	}
 
--- internal/domain/models.go
+++ internal/domain/models.go
@@ -1,10 +1,11 @@
 package domain
 
 type Movie struct {
-	ID          int    `json:"id" form:"id"`
-	Name        string `json:"name" form:"name"`
-	Description string `json:"description" form:"description"`
-	Picture     string `json:"picture" form:"picture"`
-	Video       string `json:"video" form:"video"`
-	Trailer     string `json:"trailer" form:"trailer"`
+	ID          int      `json:"id" form:"id"`
+	Name        string   `json:"name" form:"name"`
+	Description string   `json:"description" form:"description"`
+	Picture     string   `json:"picture" form:"picture"`
+	Video       string   `json:"video" form:"video"`
+	Trailer     string   `json:"trailer" form:"trailer"`
+	Genre       []string `json:"genre"`
 }
