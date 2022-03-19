aad8eb16ef282779f8ec2df4cfd9eb1c9a90ab18
nezhibitskiy <cross01rus@icloud.com>
2022-03-19 13:32:22 +0300

    Added position files

---
+++ internal/composites/staff.go
@@ -0,0 +1,27 @@
+package composites
+
+import (
+	"myapp/internal/adapters/api"
+	staff2 "myapp/internal/adapters/api/staff"
+	position2 "myapp/internal/adapters/db/position"
+	staff3 "myapp/internal/adapters/db/staff"
+	staff "myapp/internal/domain/staff"
+)
+
+type StaffComposite struct {
+	Storage staff.Storage
+	Service staff2.Service
+	Handler api.Handler
+}
+
+func NewStaffComposite(postgresComposite *PostgresDBComposite) (*StaffComposite, error) {
+	staffStorage := staff3.NewStorage(postgresComposite.DB)
+	positionStorage := position2.NewStorage(postgresComposite.DB)
+	service := staff.NewService(staffStorage, positionStorage)
+	handler := staff2.NewHandler(service)
+	return &StaffComposite{
+		Storage: staffStorage,
+		Service: service,
+		Handler: handler,
+	}, nil
+}
